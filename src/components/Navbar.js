ea90c06174e05b8a47946277815e47eac778411c
shaktigurung <mr.shakti.gurung@gmail.com>
2019-02-11 16:16:35 +1100

    Navbar completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import logo from './../logo.png';
 
 class Navbar extends Component {
   render() {
     return (
-      <div>
-        <h3>Navbar</h3>
-      </div>
+     <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <Link to="/">
+        <img src={logo} alt="cghardware" className="navbar-brand"/>
+      </Link>
+       <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+            <Link to="/" className="nav-link">
+               Products
+            </Link>
+          </li>
+       </ul> 
+       <Link to="/cart" className="ml-auto">
+          <button>
+            <i className="fas fa-cart-plus"></i>My Cart
+          </button>
+       </Link>
+     </nav>
     )
   }
 }
