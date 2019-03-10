933eab0ab57529834c778ce37776bfbaa53b56c9
shaktigurung <mr.shakti.gurung@gmail.com>
2019-03-10 18:42:20 +1100

    paypal added

--- src/context.js
+++ src/context.js
@@ -74,17 +74,49 @@ class ProductProvider extends Component {
   }
 
   increment = (id) => {
-    console.log("This is increment");
+     let tempCart = [...this.state.cart];
+     const selectedProduct = tempCart.find(item => item.id === id);
+     const index = tempCart.indexOf(selectedProduct);
+     const product = tempCart[index];
+     product.count = product.count + 1;
+     product.total = product.count * product.price;
+     this.setState(()=> {
+        return {
+            cart: [...tempCart]
+        }
+     },
+        () => { 
+            this.addTotals();
+        }
+     )
   }
 
   decrement = (id) => {
-    console.log("This is decrement");
+    let tempCart = [...this.state.cart];
+    const selectedProduct = tempCart.find(item => item.id === id);
+    const index = tempCart.indexOf(selectedProduct);
+    const product = tempCart[index];
+    product.count = product.count - 1;
+    if (product.count === 0){
+        this.removeItem(id);
+    }
+    else {
+        product.total = product.count * product.price;
+        this.setState(()=> {
+        return {
+            cart: [...tempCart]
+        }
+        },
+        () => { 
+            this.addTotals();
+        }
+        )
+    }
   }
 
   removeItem = (id) => {
     let tempProducts = [...this.state.products];
     let tempCart = [...this.state.cart];
-
     tempCart = tempCart.filter( item => item.id !== id);
     const index = tempProducts.indexOf(this.getItem(id));
     let removedProduct = tempProducts[index];
